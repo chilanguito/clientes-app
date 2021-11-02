5fc627d74f6175c75db4eeb6c4f5bc8627415833
diego.narvaez.barete.contractor <diego.narvaez.barete.contractor@bbva.com>
2021-11-02 12:46:21 -0600

    Se agrega consumo web service para  eliminar y botones con esta acción

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -15,8 +16,37 @@ export class ClientesComponent implements OnInit {
   ngOnInit() {
 
     this.clienteService.getClientes().subscribe(
-       clientes => this.clientes = clientes
+      clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    swal.fire({
+      title: '¿Está seguro?',
+      text: `¿Seguro que desea eliminar el cliente ${cliente.nombre} ${cliente.apellido}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonText: 'Si, eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+
+            this.clientes= this.clientes.filter(cli => cli !== cliente)
+
+            swal.fire('Cliente Eliminado',
+              `Cliente ${cliente.nombre} eliminado con éxito.`,
+              'success')
+          }
+        )
+
+
+      }
+    })
   }
 
 }
