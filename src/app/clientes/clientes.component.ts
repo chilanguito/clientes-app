47800c948da1e90357a25576017d18ca0e273b08
diego.narvaez.barete.contractor <diego.narvaez.barete.contractor@bbva.com>
2021-11-15 14:11:56 -0600

    Se agrega logica del paginador en componentes, vista

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -12,6 +12,7 @@ import {ActivatedRoute} from '@angular/router';
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
+  paginador: any;
 
   constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
 
@@ -30,7 +31,11 @@ export class ClientesComponent implements OnInit {
             console.log(cliente.nombre);
           });
         })
-      ).subscribe(response => this.clientes = response.content as Cliente[]);
+      ).subscribe(response =>{
+         this.clientes = response.content as Cliente[];
+         this.paginador = response;
+
+      });
     })
 
   }
