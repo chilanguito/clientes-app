ebeecf22f18533b32b3c39a640b33633294f9936
diego.narvaez.barete.contractor <diego.narvaez.barete.contractor@bbva.com>
2021-12-12 13:14:59 -0600

    Se agrega auth service para petición basic auth

---
+++ src/app/usuario/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Usuario } from './usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario: Usuario): Observable<any> {
+    const urlEndpoint = 'http://localhost:8080/oauth/token';
+
+    const credenciales = btoa('angularapp' + ':' + '12345');
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlenconded', 'Authorization': 'Basic' + credenciales });
+
+    let params = new URLSearchParams();
+
+    params.set('grand_type', 'password');
+    params.set('username', usuario.username);
+    params.set('password', usuario.password);
+
+    return this.http.post<any>(urlEndpoint, params, { headers: HttpHeaders });
+  }
+}
