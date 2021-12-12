a7c734fe318e1fed014bc058a47be2c2ea93a610
diego.narvaez.barete.contractor <diego.narvaez.barete.contractor@bbva.com>
2021-12-12 13:00:16 -0600

    Se agrega modelo de datos usuario

--- src/app/usuario/login.component.ts
+++ src/app/usuario/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from './usuario';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  titulo : string ='Por favor sign In!';
+  titulo: string = 'Por favor sign In!';
+  usuario: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+  }
 
   ngOnInit() {
+  }
+
+  login(): void {
+    console.log(this.usuario);
+
+    if (this.usuario.username == null || this.usuario.password == null) {
+      swal('Error Login', 'Username o password vacías!', 'error');
+      return;
+    }
+
   }
 
 }
